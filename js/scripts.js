4c49c00660c470afb1aa2ffadff4a8cc9689b2a7
Christine Fletcher <chrismis79@gmail.com>
2020-04-04 12:04:13 -0500

    added dev blog badge and tweaked footer styles, added 2nd portfolio piece

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,13 @@ $(document).ready(function() {
         }, 1500);
     });
 });
+
+const card1 = document.querySelector(".img1");
+card1.onclick = function() {
+    window.location.href = 'https://nasa-photo-day.netlify.com/'
+}
+
+const card2 = document.querySelector(".img2");
+card2.onclick = function() {
+    window.location.href = 'https://nychols-sweets.netlify.com/'
+}
