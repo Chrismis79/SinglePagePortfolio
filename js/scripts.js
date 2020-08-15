eaff7dddcb21b4b8515b4b9d0283d1d7ecae9c78
Christine Fletcher <chrismis79@gmail.com>
2020-08-14 21:22:17 -0500

    updated projects, more styling tweaks

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('#portfolio-button').click(function () {
         $('html, body').animate({
             scrollTop: $('#portfolio-section').offset().top
@@ -6,17 +6,22 @@ $(document).ready(function() {
     });
 });
 
+const card = document.querySelector(".img0");
+card.onclick = function () {
+    window.location.href = 'https://conways-game-of-life-christine.netlify.app'
+};
+
 const card1 = document.querySelector(".img1");
-card1.onclick = function() {
+card1.onclick = function () {
     window.location.href = 'https://nasa-photo-day.netlify.com/'
 };
 
 const card2 = document.querySelector(".img2");
-card2.onclick = function() {
+card2.onclick = function () {
     window.location.href = 'https://nychols-sweets.netlify.com/'
 };
 
 const card3 = document.querySelector(".img3");
-card3.onclick = function() {
+card3.onclick = function () {
     window.location.href = "https://swaap.co/"
 };
